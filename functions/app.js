f30378d4f20cc4db547a88deeabffa6de4c1b054
m-aung <myothuwinaung1991@gmail.com>
2021-08-04 13:30:40 -0400

    initial test for UI is created

--- functions/app.js
+++ functions/app.js
@@ -1,5 +1,6 @@
-const sum = (a,b) => {
-    // addition of 2 integers
-    return a+b
+const sum = (...input) => {
+    // storing all the inputs as an array using spread operator
+    // add all the elements from the array into one integer using array.reduce
+    return input.reduce((cur,acc)=> acc+cur)
 }
 module.exports = {sum}
---
+++ test/display.test.js
@@ -0,0 +1,6 @@
+describe('UI testing', ()=> {
+    it.todo('Test DOM')
+    it.todo('Should have user input field')
+    it.todo('Should have "+" button')
+    it.todo('Should print the result')
+})
